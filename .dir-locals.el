0e4cb424570ad69a11aab9590b2513b5029f7ae1
ajchemist <1694505+aJchemist@users.noreply.github.com>
2022-01-06 14:38:24 +0900

    Added environment.shadow-cljs

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,5 +4,4 @@
  (clojure-mode
   ;; (cider-clojure-cli-global-options . "")
   (cider-clojure-cli-aliases . "provided:test")
-  (cider-default-cljs-repl . figwheel-main)
   (clojure-local-source-test-path . "src/test")))
